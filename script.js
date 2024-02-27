fbe22ea85461999d2d3f337b7d7f74bec7afa524
Chamele <114728861+Chamele-listens@users.noreply.github.com>
2024-02-27 21:31:04 +0700

    Separate both operator and number to their own event handler
    More than 2 digit numbers can now be calculated

--- script.js
+++ script.js
@@ -31,21 +31,30 @@ function operate(firstNum,secondNum,operator){
     }
 }
 
-const buttons = document.querySelectorAll('.number, .operator')
+const NumberButtons = document.querySelectorAll('.number')
+const operatorButtons = document.querySelectorAll('.operator')
 const equal = document.querySelector(".equal")
 const clear = document.querySelector(".clear")
 const display = document.querySelector(".display")
 
 
-buttons.forEach((button)=>{
+NumberButtons.forEach((button)=>{
     button.addEventListener("click",()=> {
-        return mathValues = display.textContent += `${button.value}`
+        display.textContent += `${button.value}`
+    })
+})
+
+operatorButtons.forEach((button)=>{
+    button.addEventListener("click",() =>{
+        display.textContent += ` ${button.value} `
     })
 })
 
 equal.addEventListener("click",()=>{
+    let mathValues = display.textContent
     let sortedMath = mathValues.split(" ")
-    let answer =operate(Number(sortedMath[0]),Number(sortedMath[2]),sortedMath[1])
+    
+    let answer = operate(Number(sortedMath[0]),Number(sortedMath[2]),sortedMath[1])
     display.textContent = answer
 })
 
